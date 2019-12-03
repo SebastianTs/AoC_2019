39a6ee73adedbf982e58cfc647ae857de9e2b00e
Sebastian Tschiedel <mail@sebastian-tschiedel.de>
2019-12-03 22:44:08 +0100

    solved Day3

--- Day_03/main.go
+++ Day_03/main.go
@@ -1,48 +1,137 @@
 package main
 
 import (
-	"os"
 	"bufio"
-	"strings"
 	"fmt"
+	"os"
+	"strings"
 )
-type move struct{
-	dir rune
+
+type move struct {
+	dir  rune
 	dist int
 }
 
-type wire struct{
+type wire struct {
 	ms []move
 }
 
-type tuple struct{
+type tupel struct {
 	x int
 	y int
 }
 
-func main(){
+func main() {
 	ns, err := readInput("./input")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(len(ns))
-	findCollision(ns)
+	fmt.Printf("Solution to Part1\t%d\n", findCollision(ns))
+	fmt.Printf("Solution to Part2\t%d\n", signalDelay(ns))
 }
 
-func findCollision(wires []wire) int{
-	field := make(map[tuple]int)
-	for i, wire := range wires{
-		for _, move := range wire.ms{
-			if move.dir == 'R' {
-				fmt.Println(string(move.dir),i)
+func findCollision(wires []wire) int {
+	field := make(map[tupel][]bool)
+	for i, wire := range wires {
+		cur := tupel{0, 0}
+		for _, move := range wire.ms {
+			for j := 0; j < move.dist; j++ {
+				switch move.dir {
+				case 'R':
+					cur.x++
+				case 'L':
+					cur.x--
+				case 'U':
+					cur.y++
+				case 'D':
+					cur.y--
+				}
+				if _, inUse := field[cur]; !inUse {
+					field[cur] = make([]bool, len(wires))
+				}
+				field[cur][i] = true
+
+			}
+		}
+
+	}
+	distances := []int{}
+	for k, v := range field {
+		count := 0
+		for _, entry := range v {
+			if entry {
+				count++
+			}
+		}
+		if count > 1 {
+			distances = append(distances, abs(k.x)+abs(k.y))
+		}
+	}
+	return min(distances)
+}
+
+func signalDelay(wires []wire) int {
+	field := make(map[tupel][]bool)
+	paths := make(map[tupel][]int)
+	for i, wire := range wires {
+		cur := tupel{0, 0}
+		p := 0
+		for _, move := range wire.ms {
+			for j := 0; j < move.dist; j++ {
+				switch move.dir {
+				case 'R':
+					cur.x++
+				case 'L':
+					cur.x--
+				case 'U':
+					cur.y++
+				case 'D':
+					cur.y--
+				}
+				if _, inUse := field[cur]; !inUse {
+					field[cur] = make([]bool, len(wires))
+					paths[cur] = make([]int, len(wires))
+				}
+				field[cur][i] = true
+				p++
+				paths[cur][i] = p
 			}
 		}
 	}
-	return 0
+	distances := []int{}
+	for k, v := range field {
+		count := 0
+		for _, entry := range v {
+			if entry {
+				count++
+			}
+		}
+		if count > 1 {
+			distances = append(distances, paths[k][0]+paths[k][1])
+		}
+	}
+	return min(distances)
+}
+
+func min(m []int) int {
+	min := 1<<(32-1) - 1
+	for _, v := range m {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
 
 func readInput(filename string) (wires []wire, err error) {
-	wires = make([]wire,0)
+	wires = make([]wire, 0)
 	f, err := os.Open(filename)
 	defer f.Close()
 	if err != nil {
@@ -64,12 +153,12 @@ func readInput(filename string) (wires []wire, err error) {
 			return 0, data, bufio.ErrFinalToken
 		}
 		scntmp.Split(onComma)
-		ms := make([]move, 0)	
+		ms := make([]move, 0)
 		for scntmp.Scan() {
 			var dist int
 			cur := scntmp.Text()
-			fmt.Sscanf(cur[1:],"%d", &dist)
-			ms = append(ms, move{rune(cur[0]),dist})
+			fmt.Sscanf(cur[1:], "%d", &dist)
+			ms = append(ms, move{rune(cur[0]), dist})
 		}
 		wires = append(wires, wire{ms})
 	}

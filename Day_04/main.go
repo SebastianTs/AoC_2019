bd4f1af44732992454bbd38488905be676aa1768
Sebastian Tschiedel <mail@sebastian-tschiedel.de>
2019-12-04 21:21:45 +0100

    added Solution to Day4

--- Day_04/main.go
+++ Day_04/main.go
@@ -4,44 +4,69 @@ import (
 	"fmt"
 )
 
-const(
-	start = 168630
-	end = 718098
+const (
+	start  = 168630
+	end    = 718098
 	digits = 6
 )
 
-func main(){
-	
-	var candidates [][digits]int
-	candidates = make([][digits]int, end-start)
+func main() {
+	count, part2Count := 0, 0
 	cur := [digits]int{}
-	j:= 0
-	for i:=start; i <end; i++ {
+	for i := start; i < end; i++ {
 		cur = intToArray(i)
-		candidates[j] = cur
-		j++
+		if meetsCriteria(cur) {
+			count++
+		}
+		if meetsCriteriaExactDouble(cur) {
+			part2Count++
+		}
 	}
-	fmt.Println(len(candidates))
-	removeFromList(0, candidates)
-	fmt.Println(candidates[0])
+	fmt.Printf("Solution 1:\t %d\nSolution 2:\t%d\n", count, part2Count)
 }
 
-func hasDouble(lst [digits]int) bool{
-	for i:=0; i < 10; i++{
-		fmt.Println(i)
-	} 
-	return false
-
+func meetsCriteria(lst [digits]int) bool {
+	hasDouble := false
+	notDecrasing := false
+	for i := 0; i < len(lst)-1; i++ {
+		if lst[i] == lst[i+1] {
+			hasDouble = true
+		}
+		notDecrasing = lst[i] <= lst[i+1]
+		if !notDecrasing {
+			break
+		}
+	}
+	return hasDouble && notDecrasing
 }
 
-func removeFromList(i int, lst [][digits]int){
-	lst[i] = lst[len(lst)-1]
-	lst[len(lst)-1] = [digits]int{}
-	lst = lst[:len(lst)-1]
+func meetsCriteriaExactDouble(lst [digits]int) bool {
+	hasDouble := false
+	notDecrasing := false
+	count := map[int]int{}
+	doubles := []int{}
+	for i := 0; i < len(lst); i++ {
+		count[lst[i]]++
+	}
+	for i := 0; i < len(lst)-1; i++ {
+		if lst[i] == lst[i+1] {
+			doubles = append(doubles, lst[i])
+		}
+		notDecrasing = lst[i] <= lst[i+1]
+		if !notDecrasing {
+			break
+		}
+	}
+	for _, val := range doubles {
+		if count[val] == 2 {
+			hasDouble = true
+			break
+		}
+	}
+	return hasDouble && notDecrasing
 }
-
-func intToArray(number int) (out [digits]int){
-	for i:= 0; i < digits; i++{
+func intToArray(number int) (out [digits]int) {
+	for i := 0; i < digits; i++ {
 		out[digits-i-1] = number % 10
 		number /= 10
 	}
